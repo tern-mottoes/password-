86a9ca46628d2273617dd0f6e1c8414535cdf2d5
Stasy Brock <152871506+tern-mottoes@users.noreply.github.com>
2023-12-06 11:35:14 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+# main.py
+
+from password_manager import PasswordManager
+
+def main():
+    password_manager = PasswordManager()
+    password_manager.run()
+
+if __name__ == "__main__":
+    main()
