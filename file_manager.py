e0da89b5bd736f83ccb988a5bee0132d12c3da58
Stasy Brock <152871506+tern-mottoes@users.noreply.github.com>
2023-12-06 11:35:52 +0600

    Create file_manager.py

---
+++ file_manager.py
@@ -0,0 +1,10 @@
+# file_manager.py
+
+class FileManager:
+    def read_file(self, filename):
+        # File reading logic here
+        pass
+
+    def write_file(self, filename, data):
+        # File writing logic here
+        pass
