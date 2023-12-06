ae29668c000b471df77011c1ae318985fe2a7b5e
Stasy Brock <152871506+tern-mottoes@users.noreply.github.com>
2023-12-06 11:35:42 +0600

    Create password_generator.py

---
+++ password_generator.py
@@ -0,0 +1,9 @@
+# password_generator.py
+
+import random
+import string
+
+class PasswordGenerator:
+    def generate_password(self, length=12):
+        # Password generation logic here
+        pass
